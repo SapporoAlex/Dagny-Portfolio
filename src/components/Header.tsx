59c2775a7ce891fd7fe02d4a304b64441ff515e6
Alexander McKinley <alexanderfromaustralia@gmail.com>
2025-06-29 22:28:24 +0900

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,12 @@ function Header() {
       <header>
         <h1>Dagny Strand</h1>
         <p>Visual Artist</p>
-
-        <a
-          id="instagram-link"
-          href="https://www.instagram.com/dagnystrand"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-instagram"></i>
-        </a>
         <a
           className="btn"
-          onClick={() =>
-            document
-              .getElementById("galleries-start")
-              ?.scrollIntoView({ behavior: "smooth" })
-          }
+          href="https://www.instagram.com/dagnystrand"
+          target="_blank"
         >
-          View My Work
+          Contact Me
         </a>
       </header>
     </>
